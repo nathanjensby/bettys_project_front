5e075744283ef70fe6c4e1463bb5f44878c81d8c
Nathan Jensby <njensby@gmail.com>
2017-02-08 14:53:52 -0600

    added categoriescontainer and category models

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CategoriesContainer from './components/CategoriesContainer';
+import Category from './components/Category';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +10,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the Recipe Box</h2>
         </div>
+        <CategoriesContainer>
+
+        </CategoriesContainer>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Category.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Category extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am a category!</h1>
+      </div>
+    )
+  }
+}
+export default Category;
