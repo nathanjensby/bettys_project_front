79bb0b09bb6effb2e315c19fef66f6f7e366da9c
Nathan Jensby <njensby@gmail.com>
2017-02-23 15:26:04 -0600

    removed components, starting fresh with flux. NPM INSTALL FLUX

--- src/views/App.js
+++ src/views/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CategoriesContainer from './components/CategoriesContainer';
 import RecipeCardsContainer from './components/RecipeCardsContainer';
 import SearchBar from './components/SearchBar';
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   constructor(props) {
